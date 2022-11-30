933343070291b667a023f5872a70d944de511b6d
Pierre-Louis <plvergely@gmail.com>
2022-11-30 14:15:04 +0100

    initial commit

---
+++ BoardGameTP/BoardGameTPApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardGameTPApp.swift
+//  BoardGameTP
+//
+//  Created by Pierre-Louis Vergely on 30/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct BoardGameTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
